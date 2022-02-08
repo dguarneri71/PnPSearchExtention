1952ba49b9529fe507004afbd1fbf781ca0b57e2
Daniele Guarneri <dguarneri71@outlook.com>
2022-02-08 18:57:30 +0100

    nuovi test su custom component

--- msys-extention/src/libraries/msysSearchLibrary/WebComponents/Demo/CustomComponent.tsx
+++ msys-extention/src/libraries/msysSearchLibrary/WebComponents/Demo/CustomComponent.tsx
@@ -2,64 +2,41 @@ import * as React from 'react';
 import { BaseWebComponent } from '@pnp/modern-search-extensibility';
 import * as ReactDOM from 'react-dom';
 
-export interface IObjectParam {
-    myProperty: string;
-}
-
 export interface ICustomComponentProps {
-
-    /**
-     * A sample string param
-     */
-    myStringParam?: string;
-
-    /**
-     * A sample object param
-     */
-    myObjectParam?: IObjectParam;
-
-    /**
-     * A sample date param
-     */
-    myDateParam?: Date;
-
-    /**
-     * A sample number param
-     */
-    myNumberParam?: number;
-
-    /**
-     * A sample boolean param
-     */
-    myBooleanParam?: boolean;
+    content?: {};
 }
 
 export interface ICustomComponenState {
 }
 
 export class CustomComponent extends React.Component<ICustomComponentProps, ICustomComponenState> {
-    
+
     public render() {
 
         // Parse custom object
-        //const myObject: IObjectParam = this.props.myObjectParam;
+        console.log("Content: ", this.props.content);
+        let items:any[] = this.props.content["data"]["items"];
+        console.log("Items: ", items);
+        let count = items.length;
+        let currentPage = this.props.content["paging"]["currentPageNumber"];
 
-        return  <div>
-                    {this.props.myStringParam}
-                </div>;
+        return <div>
+            <span>Ciao: {count} - page: {currentPage}</span>
+        </div>;
     }
 }
 
+//<msys-custom-component data-content="{{JSONstringify this 2}}"/>
 export class MyCustomComponentWebComponent extends BaseWebComponent {
-   
+
     public constructor() {
-        super(); 
+        super();
     }
- 
+
     public async connectedCallback() {
- 
-       let props = this.resolveAttributes();
-       const customComponent = <CustomComponent {...props}/>;
-       ReactDOM.render(customComponent, this);
-    }    
+
+        let props = this.resolveAttributes();
+        const customComponent = <CustomComponent {...props} />;
+        ReactDOM.render(customComponent, this);
+    }
 }
